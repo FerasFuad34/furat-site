7e753a70dc1e10a98ca14abe8783da740be8092e
Furat Dev <dev@furat.local>
2026-09-22 03:45:15 +0000

    admin 24: remove overflow-x hidden kill-switch (broke sticky sidebar); motion detox from 23 remains

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/manifest.webmanifest', (req, res) => {
   });
 });
 
-const ADMIN_BUILD = 23;
+const ADMIN_BUILD = 24;
 app.get('/api/build', (req, res) => { res.set('Cache-Control', 'no-store'); res.json({ admin: ADMIN_BUILD }); });
 app.get('/sw.js', (req, res) => {
   res.type('application/javascript').send(`
